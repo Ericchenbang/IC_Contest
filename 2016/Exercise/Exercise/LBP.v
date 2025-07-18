0d590d279b646a356621a111e3e65609f2c52efa
Ericchenbang <ericchenbsng@gmail.com>
2025-07-18 17:40:11 +0800

    Second wrote of 2016, improving area and timing

--- 2016/Exercise/Exercise/LBP.v
+++ 2016/Exercise/Exercise/LBP.v
@@ -11,146 +11,105 @@ output  reg [14:0] 	lbp_addr;
 output  reg 	    lbp_valid;
 output  reg [7:0] 	lbp_data;
 output  reg	        finish;
-
-
 //====================================================================
 
-// 129~16254
-reg [13:0] lbpIndex;
-// 0~8
-reg [3:0] lbpCounter;
-reg [3:0] localCounter;
-// record the nine data
-reg [7:0] data [0:8];
+wire ending;
+assign ending = ((gray_addr + 1) & 7'b1111111) == 0;
 
-reg [7:0] lbpData;
-reg [7:0] tempLbpData;
+reg [3:0] count;
+reg [7:0] compare;
+wire [7:0] multi;
+assign multi = count < 4 ? 8'b1 << count : 8'b1 << (count - 1);
 
+
+wire [8:0] rowTerm;
+wire [1:0] colTerm;
+assign rowTerm = (count <= 2 ? 0: (count <= 5 ? 128: 256));
+assign colTerm = (count % 3);
+
+parameter StandBy = 0, ReqData = 1, Output = 2;
 reg [1:0] state;
-parameter   LoadData = 2'd0,
-            CountLbp = 2'd1,
-            OutputLbp = 2'd2,
-            Restore = 2'd3;
-reg passIndex;
 
-always@(posedge clk, posedge reset) begin
+always@(posedge clk, posedge reset)begin
     if (reset) begin
-        gray_req <= 0;
+        state <= StandBy;
+        gray_addr <= 129;
+
+        gray_req <= 1;
         lbp_valid <= 0;
         finish <= 0;
-
-        lbpCounter <= 0;
-        state <= LoadData;
-
-        passIndex <= 1;
     end
-    else if ((gray_ready && gray_req == 0) && state == LoadData) begin
-        gray_req <= 1;
-        state <= LoadData;
+    else if (gray_ready && state == StandBy) begin
+        state <= ReqData;
+        compare <= gray_data;
+        gray_addr <= gray_addr - 129;
 
-        lbpIndex <= 14'd129;
-        lbpCounter <= 0;
-
-        passIndex <= 1;
+        count <= 0;
+        lbp_data <= 0;
     end
-    else if (lbpIndex < 14'd16255) begin
-        if (state == LoadData) begin
-            gray_req <= 1;
-            lbp_valid <= 0;
-
-            if (lbpCounter < 4'd9) begin
-                if (passIndex) begin
-                    case (lbpCounter)
-                        4'd0: gray_addr <= lbpIndex - 14'd129;
-                        4'd1: gray_addr <= lbpIndex - 14'd128;
-                        4'd2: gray_addr <= lbpIndex - 14'd127;
-                        4'd3: gray_addr <= lbpIndex - 14'd1;
-                        4'd4: gray_addr <= lbpIndex;
-                        4'd5: gray_addr <= lbpIndex + 14'd1;
-                        4'd6: gray_addr <= lbpIndex + 14'd127;
-                        4'd7: gray_addr <= lbpIndex + 14'd128;
-                        4'd8: gray_addr <= lbpIndex + 14'd129;
-                        default: gray_addr <= 0;
-                    endcase
-                    localCounter <= lbpCounter;
-                    data[localCounter] <= gray_data;
-                    lbpCounter <= lbpCounter + 1;
-                end
-                else begin
-                    case (lbpCounter)
-                        4'd2: begin
-                            gray_addr <= lbpIndex - 14'd127;
-                            data[0] <= data[1];
-                            data[1] <= data[2];
-                            data[3] <= data[4];
-                            data[4] <= data[5];
-                            data[6] <= data[7];
-                            data[7] <= data[8];
-                        end
-                        4'd5: gray_addr <= lbpIndex + 14'd1;
-                        4'd8: gray_addr <= lbpIndex + 14'd129;
-                        default: gray_addr <= 0;
-                    endcase
-                    localCounter <= lbpCounter;
-                    data[localCounter] <= gray_data;
-                    lbpCounter <= lbpCounter + 3;
-                end
+    else if (state == ReqData) begin
+        if (count <= 8) begin
+            if (gray_data >= compare) begin
+                lbp_data <= lbp_data + multi;
             end
             else begin
-                localCounter <= lbpCounter;
-                data[localCounter] <= gray_data;
-                gray_req <= 0;
-                state <= OutputLbp;
-            end         
-        end
-        else if (state == OutputLbp) begin
-            if (lbp_valid == 0) begin
-                lbpData <= tempLbpData;
-                lbp_valid <= 1;
-            end
-            else begin
-                lbp_addr <= lbpIndex;
-                lbp_data <= lbpData;
-
-                lbpCounter <= 0;
-                        
-                if ((lbpIndex + 14'd2) % 14'd128 == 0)
-                    lbpIndex <= lbpIndex + 14'd3;
-                else 
-                    lbpIndex <= lbpIndex + 14'd1;
-                state <= Restore;
+                lbp_data <= lbp_data + 0;
             end
         end
         else begin
-            state <= LoadData;
-            
-            if ((lbpIndex - 1) % 128 == 0) begin
-                passIndex <= 1;
-                lbpCounter <= 0;
+            lbp_data <= lbp_data + 0;
+        end
+
+        case(count) 
+            0, 1, 6, 7: begin
+                gray_addr <= gray_addr + 1;
+                count <= count + 1;
             end
-            else begin
-                passIndex <= 0;
-                lbpCounter <= 2;
+            2, 5: begin
+                gray_addr <= gray_addr + 126;
+                count <= count + 1;
             end
-                
+            3: begin
+                gray_addr <= gray_addr + 2;
+                count <= 5;
+            end
+            8: begin
+                gray_req <= 0;
+                count <= count + 1;
+            end
+            default: begin 
+                gray_addr <= gray_addr + 0;
+                state <= Output;
+
+                lbp_valid <= 1;
+                lbp_addr <= gray_addr - 129;
+            end
+        endcase
+    end
+    else if (state == Output) begin
+        if (gray_addr == 16383) begin
+            lbp_valid <= 0;
+            finish <= 1;
+        end
+        else begin
+            if (ending) begin
+                gray_addr <= gray_addr - 126;
+            end
+            else begin    
+                gray_addr <= gray_addr - 128;
+            end
+
+            state <= StandBy;
             gray_req <= 1;
             lbp_valid <= 0;
         end
     end
-    else
-        finish <= 1;
+    else begin
+        gray_req <= 0;
+        lbp_valid <= 0;
+        finish <= 0;
+    end
 end
 
-
-always@(*) begin
-    tempLbpData[0] = (data[4] <= data[0]);
-    tempLbpData[1] = (data[4] <= data[1]);
-    tempLbpData[2] = (data[4] <= data[2]);
-    tempLbpData[3] = (data[4] <= data[3]);
-    tempLbpData[4] = (data[4] <= data[5]);
-    tempLbpData[5] = (data[4] <= data[6]);
-    tempLbpData[6] = (data[4] <= data[7]);
-    tempLbpData[7] = (data[4] <= data[8]);
-end
 //====================================================================
 endmodule
