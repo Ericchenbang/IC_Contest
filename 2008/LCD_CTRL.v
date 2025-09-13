1021b727cb9ec6913af5153aa606ea0391677129
Ericchenbang <ericchenbsng@gmail.com>
2025-09-13 12:38:10 +0800

    Second version of 2008

--- 2008/LCD_CTRL.v
+++ 2008/LCD_CTRL.v
@@ -8,142 +8,206 @@ output reg [7:0] dataout;
 output reg output_valid;
 output reg busy;
 
-reg checkCommand;
-reg [2:0] command;
-reg [7:0] data[8:0][11:0];
-reg [3:0] lOrigin;
-reg [3:0] wOrigin;
+parameter ReceiveCmd = 0, LoadData = 1, ZoomFitOutput = 2, ZoomInOutput = 3, ShiftRight = 4, ShiftLeft = 5, ShiftUp = 6, ShiftDown = 7;
+reg [2:0] state;
+reg zoomFit;
 
-reg [6:0] inputCount;
+// LoadData
+reg [7:0] data [0:8][0:11];
+reg [3:0] dataRow;
+reg [3:0] dataCol;
+
+// Output
 reg [4:0] outputCount;
-
-reg state;
-
-parameter   LoadData = 3'd0,
-            ZoomIn = 3'd1,
-            ZoomFit = 3'd2,
-            ShiftRight = 3'd3,
-            ShiftLeft = 3'd4,
-            ShiftUp = 3'd5,
-            ShiftDown = 3'd6,
-            DoNothing = 3'd7,
-            ZoomInState = 1,
-            ZoomFitState = 0;
+reg [4:0] originRow;
+reg [4:0] originCol;
 
 always@(posedge clk, posedge reset) begin
     if (reset) begin
-        command <= DoNothing;
-        checkCommand <= 0;
+        state <= ReceiveCmd;
+
+        
+        outputCount <= 0;
+        originRow <= 3;
+        originCol <= 4;
+
         busy <= 0;
         output_valid <= 0;
-        inputCount <= 0;
-        outputCount <= 0;
-        state <= ZoomFitState;
     end
-    else if (cmd_valid && busy == 0) begin
-        checkCommand <= 1;
-        command <= cmd;
-        busy <= 1;
+    else if (state == ReceiveCmd) begin
+        if (cmd_valid) begin
+            busy <= 1;
+            case(cmd)
+                0: begin
+                    state <= LoadData;
+                    dataRow <= 0;
+                    dataCol <= 0;
+                    zoomFit <= 1;
+                    originRow <= 3;
+                    originCol <= 4;
+                end
+                2: begin
+                    state <= ZoomFitOutput;
+                    dataRow <= 1;
+                    dataCol <= 1;
+                    originRow <= 3;
+                    originCol <= 4;
+                end
+                1: begin
+                    state <= ZoomInOutput;
+                    zoomFit <= 0;
+                    if (zoomFit) begin
+                        dataRow <= 3;
+                        dataCol <= 4;
+                    end
+                    else begin
+                        dataRow <= originRow;
+                        dataCol <= originCol;
+                    end
+                end
+                3, 4, 5, 6: begin
+                    if (zoomFit) begin
+                        state <= ZoomFitOutput;
+                        dataRow <= 1;
+                        dataCol <= 1;
+                    end
+                    else begin
+                        state <= cmd + 1;
+                    end
+                end
+                default: begin
+                    state <= ReceiveCmd;
+                end
+            endcase
+        end
     end
-    else if (checkCommand) begin
-        case(command)
-            LoadData: begin
-                if (inputCount < 7'd108) begin
-                    data[inputCount / 12][inputCount % 12] <= datain;
-                    inputCount <= inputCount + 1;
-                end
-                else begin
-                    inputCount <= 0;
-                    command <= ZoomFit;
-                end
+    else if (state == LoadData) begin
+        data[dataRow][dataCol] <= datain;
+
+        if (dataCol == 11) begin
+            if (dataRow == 8) begin
+                state <= ZoomFitOutput;
+                dataRow <= 1;
+                dataCol <= 1;
             end
-            ZoomIn: begin
-                state <= ZoomInState;
-                if (outputCount < 5'd16) begin
-                    output_valid <= 1;
-                    dataout <= data[wOrigin-4'd2 + (outputCount >> 2)][lOrigin-4'd2 + outputCount%4];
-                    outputCount <= outputCount + 1;
-                end
-                else begin
-                    //command <= DoNothing;
-                    checkCommand <= 0;
-                    outputCount <= 0;
-                    output_valid <= 0;
-                    busy <= 0;
-                end
+            else begin
+                dataRow <= dataRow + 1;
+                dataCol <= 0;
             end
-            ZoomFit: begin
-                state <= ZoomFitState;
-                if (outputCount < 5'd16) begin 
-                    output_valid <= 1;
-                    dataout <= data[((outputCount >> 2) << 1) + 1][(outputCount%4)*3 + 1];
-                    outputCount <= outputCount + 1;
-                end
-                else begin
-                    //command <= DoNothing;
-                    checkCommand <= 0;
-                    outputCount <= 0;
-                    output_valid <= 0;
-                    busy <= 0;
-                    lOrigin <= 4'd6;
-                    wOrigin <= 4'd5;
-                end
-            end
-            ShiftRight: begin
-                if (state == ZoomInState) begin
-                    if (lOrigin < 4'ha)
-                        lOrigin <= lOrigin + 4'd1;
-                    else
-                        lOrigin <= lOrigin;
-                    command <= ZoomIn;
-                end
-                else begin
-                    command <= ZoomFit;
-                end
-            end
-            ShiftLeft: begin
-                if (state == ZoomInState) begin
-                    if (lOrigin > 4'h2)
-                        lOrigin <= lOrigin - 4'd1;
-                    else
-                        lOrigin <= lOrigin;
-                    command <= ZoomIn;
-                end
-                else begin
-                    command <= ZoomFit;
-                end
-            end
-            ShiftUp: begin
-                if (state == ZoomInState) begin
-                    if (wOrigin > 4'h2)
-                        wOrigin <= wOrigin - 4'd1;
-                    else
-                        wOrigin <= wOrigin;
-                    command <= ZoomIn;
-                end
-                else begin
-                    command <= ZoomFit;
-                end
-            end
-            ShiftDown: begin
-                if (state == ZoomInState) begin
-                    if (wOrigin < 4'h7)
-                        wOrigin <= wOrigin + 4'd1;
-                    else
-                        wOrigin <= wOrigin;
-                    command <= ZoomIn;
-                end
-                else begin
-                    command <= ZoomFit;
-                end
-            end
-            /*default: begin
-                output_valid <= 0;
-                busy <= 0;
-            end*/
-        endcase
+        end
+        else begin
+            dataCol <= dataCol + 1;
+        end
+
     end
-    else 
-        output_valid <= 0;
+    else if (state == ZoomFitOutput) begin
+        if (outputCount <= 15) begin
+            if (outputCount == 0) begin
+                output_valid <= 1;
+            end
+
+            dataout <= data[dataRow][dataCol];
+
+            outputCount <= outputCount + 1;
+
+            if (dataCol == 10) begin
+                if (dataRow != 7) begin
+                    dataCol <= 1;
+                    dataRow <= dataRow + 2;
+                end
+            end
+            else begin
+                dataCol <= dataCol + 3;
+            end
+        end
+        else begin
+            busy <= 0;
+            output_valid <= 0;
+            state <= ReceiveCmd;
+            outputCount <= 0;
+
+        end
+    end
+    else if (state == ZoomInOutput) begin
+        if (outputCount <= 15) begin
+            if (outputCount == 0) begin
+                output_valid <= 1;
+            end
+
+            dataout <= data[dataRow][dataCol];
+
+            outputCount <= outputCount + 1;
+
+            if (dataCol == originCol + 3) begin
+                if (dataRow != originRow + 3) begin
+                    dataCol <= originCol;
+                    dataRow <= dataRow + 1;
+                end
+            end
+            else begin
+                dataCol <= dataCol + 1;
+            end
+        end
+        else begin
+            busy <= 0;
+            output_valid <= 0;
+            state <= ReceiveCmd;
+            outputCount <= 0;
+        end
+    end
+    else if (state == ShiftRight) begin
+        state <= ZoomInOutput;
+        
+        if (originCol < 8) begin
+            originCol <= originCol + 1;
+            dataCol <= originCol + 1;
+        end
+        else begin
+            dataCol <= originCol;
+        end
+        dataRow <= originRow;
+    end
+    else if (state == ShiftLeft) begin
+        state <= ZoomInOutput;
+        
+        if (originCol > 0) begin
+            originCol <= originCol - 1;
+            dataCol <= originCol - 1;
+        end
+        else begin
+            dataCol <= originCol;
+        end
+        dataRow <= originRow;
+    end
+    else if (state == ShiftUp) begin
+        state <= ZoomInOutput;
+        
+        if (originRow > 0) begin
+            originRow <= originRow - 1;
+            dataRow <= originRow - 1;
+        end
+        else begin
+            dataRow <= originRow;
+        end
+        dataCol <= originCol;
+    end
+    else if (state == ShiftDown) begin
+        state <= ZoomInOutput;
+        
+        if (originRow < 5) begin
+            originRow <= originRow + 1;
+            dataRow <= originRow + 1;
+        end
+        else begin
+            dataRow <= originRow;
+        end
+        dataCol <= originCol;
+    end
 end
+
+
+
+
+
+
 endmodule
